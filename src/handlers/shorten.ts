5eb7af844691a28b7a29ba6d52b8a579b9a589bb
Gabriel Ting <gabrielfting@gmail.com>
2021-01-12 19:50:24 -0600

    Fix RegEx

--- src/handlers/shorten.ts
+++ src/handlers/shorten.ts
@@ -6,30 +6,28 @@ import qs from 'qs'
 // }
 
 const parseShortenString = (text: string): RegExpMatchArray | null => {
-  return text.match('?<path>w*)s+(?<url>')
+  return text.match('(?<path>\w*)\s+(?<url>.*)')
 }
 
 const postNewUrl = (path: string, url: string): Promise<Response> => {
-    const fetchUrl = `https://vhl.ink`
-    const body = new URLSearchParams()
-    body.set("url", url)
-    body.set("path", path)
-    const headers = new Headers({
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'x-preshared-key': SECRET_KEY
-    })
-    return fetch(fetchUrl, { body, headers })
+  const fetchUrl = `https://vhl.ink`
+  const body = new URLSearchParams()
+  body.set('url', url)
+  body.set('path', path)
+  const headers = new Headers({
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'x-preshared-key': SECRET_KEY,
+  })
+  return fetch(fetchUrl, { body, headers })
 }
 
-const compact = (array: string[]) => array.filter(el => el)
+const compact = (array: string[]) => array.filter((el) => el)
 
 export const constructSlackMessage = (text: string) => {
-//   const issue_link = `<${issue.html_url}|${issue_string}>`
-//   const user_link = `<${issue.user.html_url}|${issue.user.login}>`
+  //   const issue_link = `<${issue.html_url}|${issue_string}>`
+  //   const user_link = `<${issue.user.html_url}|${issue.user.login}>`
 
-  const text_lines = [
-    text
-  ]
+  const text_lines = [text]
 
   return [
     {
@@ -54,7 +52,9 @@ export default async (request: Request) => {
 
     // Ensure that we process a valid string
     if (typeof text === 'string') {
-      const params: { [key: string]: string } | undefined = parseShortenString(text)?.groups
+      const params: { [key: string]: string } | undefined = parseShortenString(
+        text,
+      )?.groups
       const path: string | undefined = params?.path
       const url: string | undefined = params?.url
 
@@ -65,15 +65,15 @@ export default async (request: Request) => {
         const blocks = constructSlackMessage(text)
 
         return new Response(
-            JSON.stringify({
+          JSON.stringify({
             blocks,
             response_type: 'in_channel',
-            }),
-            { headers: { 'Content-type': 'application/json' } },
+          }),
+          { headers: { 'Content-type': 'application/json' } },
         )
       }
 
-      throw new Error();
+      throw new Error()
     }
     throw new Error('Expected a string to be included in request.')
   } catch (err) {
