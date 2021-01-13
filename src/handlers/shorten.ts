31aa83fad9ec755d92fac86cc24cf043425153f1
Matt Leon <ml@mattleon.com>
2021-01-13 17:27:50 -0500

    Fix URLSearchParams usage

--- src/handlers/shorten.ts
+++ src/handlers/shorten.ts
@@ -4,36 +4,39 @@
 // }
 
 const parseShortenString = (text: string): RegExpMatchArray | null => {
-  return text.trim().match('(?<path>\w*)\s+(?<url>[\S]+)')
-}
+  return text.trim().match("(?<path>\w*)\s+(?<url>[\S]+)");
+};
 
 const postNewUrl = (path: string, url: string): Promise<Response> => {
-  const fetchUrl = `https://vhl.ink`
+  const fetchUrl = `https://vhl.ink`;
   const headers = new Headers({
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'x-preshared-key': SECRET_KEY,
-  })
-  return fetch(fetchUrl + new URLSearchParams({url, path}), { headers })
-}
+    "Content-Type": "application/x-www-form-urlencoded",
+    "x-preshared-key": SECRET_KEY,
+  });
+  return fetch(
+    `${fetchUrl}?${new URLSearchParams({ url, path }).toString()}`,
+    { headers },
+  );
+};
 
-const compact = (array: string[]) => array.filter((el) => el)
+const compact = (array: string[]) => array.filter((el) => el);
 
 export const constructSlackMessage = (text: string) => {
   //   const issue_link = `<${issue.html_url}|${issue_string}>`
   //   const user_link = `<${issue.user.html_url}|${issue.user.login}>`
 
-  const text_lines = [text]
+  const text_lines = [text];
 
   return [
     {
-      type: 'section',
+      type: "section",
       text: {
-        type: 'mrkdwn',
-        text: compact(text_lines).join('\n'),
+        type: "mrkdwn",
+        text: compact(text_lines).join("\n"),
       },
     },
-  ]
-}
+  ];
+};
 
 /**
  * Handles shortening links. Expects the text to look like:
@@ -41,38 +44,38 @@ export const constructSlackMessage = (text: string) => {
  */
 export default async (request: Request) => {
   try {
-    const queryParams = new URL(request.url).searchParams
-    const text = queryParams.get('text')
+    const queryParams = new URL(request.url).searchParams;
+    const text = queryParams.get("text");
 
     // Ensure that we process a valid string
-    if (typeof text === 'string') {
+    if (typeof text === "string") {
       const params: { [key: string]: string } | undefined = parseShortenString(
         text,
-      )?.groups
-      const path: string | undefined = params?.path
-      const url: string | undefined = params?.url
+      )?.groups;
+      const path: string | undefined = params?.path;
+      const url: string | undefined = params?.url;
 
       if (path && url) {
-        const response = await postNewUrl(path, url)
-        const res = await response.json()
+        const response = await postNewUrl(path, url);
+        const res = await response.json();
 
-        const blocks = constructSlackMessage(text)
+        const blocks = constructSlackMessage(text);
 
         return new Response(
           JSON.stringify({
             blocks,
-            response_type: 'in_channel',
+            response_type: "in_channel",
           }),
-          { headers: { 'Content-type': 'application/json' } },
-        )
+          { headers: { "Content-type": "application/json" } },
+        );
       }
 
-      throw new Error()
+      throw new Error();
     }
-    throw new Error('Expected a string to be included in request.')
+    throw new Error("Expected a string to be included in request.");
   } catch (err) {
     const errorText =
-      'The request failed, please ensure that you are supplying options in the format `/shorten <path> <url>`.'
-    return new Response(err.message || errorText)
+      "The request failed, please ensure that you are supplying options in the format `/shorten <path> <url>`.";
+    return new Response(err.message || errorText);
   }
-}
+};
