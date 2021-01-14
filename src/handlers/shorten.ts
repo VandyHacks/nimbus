ec050a7f9e704ac783ff9403a80b460db6bc16be
Gabriel Ting <gabrielfting@gmail.com>
2021-01-13 22:43:57 -0600

    Fix RegEx

--- src/handlers/shorten.ts
+++ src/handlers/shorten.ts
@@ -3,39 +3,37 @@
 //     url: string
 // }
 
+const shortenRegex = /(?<path>\w*)\s+(?<url>[\S]+)/;
+
 const parseShortenString = (text: string): RegExpMatchArray | null => {
-  return text.trim().match("(?<path>\w*)\s+(?<url>[\S]+)");
+	return text.trim().match(shortenRegex);
 };
 
 const postNewUrl = (path: string, url: string): Promise<Response> => {
-  const fetchUrl = `https://vhl.ink`;
-  const headers = new Headers({
-    "Content-Type": "application/x-www-form-urlencoded",
-    "x-preshared-key": SECRET_KEY,
-  });
-  return fetch(
-    `${fetchUrl}?${new URLSearchParams({ url, path }).toString()}`,
-    { headers },
-  );
+	const fetchUrl = `https://vhl.ink`;
+	const headers = new Headers({
+		'Content-Type': 'application/x-www-form-urlencoded',
+		'x-preshared-key': SECRET_KEY,
+	});
+	return fetch(`${fetchUrl}?${new URLSearchParams({ url, path }).toString()}`, {
+		headers,
+	});
 };
 
-const compact = (array: string[]) => array.filter((el) => el);
+const compact = (array: string[]) => array.filter(el => el);
 
 export const constructSlackMessage = (text: string) => {
-  //   const issue_link = `<${issue.html_url}|${issue_string}>`
-  //   const user_link = `<${issue.user.html_url}|${issue.user.login}>`
+	const text_lines = [text];
 
-  const text_lines = [text];
-
-  return [
-    {
-      type: "section",
-      text: {
-        type: "mrkdwn",
-        text: compact(text_lines).join("\n"),
-      },
-    },
-  ];
+	return [
+		{
+			type: 'section',
+			text: {
+				type: 'mrkdwn',
+				text: compact(text_lines).join('\n'),
+			},
+		},
+	];
 };
 
 /**
@@ -43,39 +41,39 @@ export const constructSlackMessage = (text: string) => {
  * `/shorten <path> <url>`, and result in `vhl.ink/path`
  */
 export default async (request: Request) => {
-  try {
-    const queryParams = new URL(request.url).searchParams;
-    const text = queryParams.get("text");
+	try {
+		const queryParams = new URL(request.url).searchParams;
+		const text = queryParams.get('text');
 
-    // Ensure that we process a valid string
-    if (typeof text === "string") {
-      const params: { [key: string]: string } | undefined = parseShortenString(
-        text,
-      )?.groups;
-      const path: string | undefined = params?.path;
-      const url: string | undefined = params?.url;
+		// Ensure that we process a valid string
+		if (typeof text === 'string') {
+			const params: { [key: string]: string } | undefined = parseShortenString(
+				text,
+			)?.groups;
+			const path: string | undefined = params?.path;
+			const url: string | undefined = params?.url;
 
-      if (path && url) {
-        const response = await postNewUrl(path, url);
-        const res = await response.json();
+			if (path && url) {
+				const response = await postNewUrl(path, url);
+				const res = await response.json();
 
-        const blocks = constructSlackMessage(text);
+				const blocks = constructSlackMessage(text);
 
-        return new Response(
-          JSON.stringify({
-            blocks,
-            response_type: "in_channel",
-          }),
-          { headers: { "Content-type": "application/json" } },
-        );
-      }
+				return new Response(
+					JSON.stringify({
+						blocks,
+						response_type: 'in_channel',
+					}),
+					{ headers: { 'Content-type': 'application/json' } },
+				);
+			}
 
-      throw new Error();
-    }
-    throw new Error("Expected a string to be included in request.");
-  } catch (err) {
-    const errorText =
-      "The request failed, please ensure that you are supplying options in the format `/shorten <path> <url>`.";
-    return new Response(err.message || errorText);
-  }
+			throw new Error();
+		}
+		throw new Error('Expected a string to be included in request.');
+	} catch (err) {
+		const errorText =
+			'The request failed, please ensure that you are supplying options in the format `/shorten <path> <url>`.';
+		return new Response(err.message || errorText);
+	}
 };
