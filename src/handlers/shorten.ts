c141c1fa915023140b27cea41d070bc0db056759
Gabriel Ting <gabrielfting@gmail.com>
2021-01-14 00:04:11 -0600

    Add draft of slack request validation

--- src/handlers/shorten.ts
+++ src/handlers/shorten.ts
@@ -49,15 +49,49 @@ export const constructSlackMessage = (text: string) => {
 	];
 };
 
+const validSlackRequest = async (request: Request): Promise<boolean> => {
+  try {
+    // Grab raw body
+    const requestBody = request.body;
+    const timestamp = request.headers.get('X-Slack-Request-Timestamp')
+
+    // Protect against replay attacks by checking if it's a request that's older than 5 minutes
+    if (timestamp && Date.now() - new Date(timestamp).getTime() > 5 * 60 * 1000) {
+      throw new Error("The request is old.")
+    }
+
+    const sigBasestring = `v0:${timestamp}:${requestBody}`;
+
+    // Hash the basestring using signing secret as key, taking hex digest of hash. Uses Cloudflare's Web Crypto https://developers.cloudflare.com/workers/runtime-apis/web-crypto
+    const msgUint8 = new TextEncoder().encode(sigBasestring)
+    // Refer to https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest#converting_a_digest_to_a_hex_string
+    const hashBuffer = await crypto.subtle.digest("SHA-256", msgUint8)
+    const hashArray = Array.from(new Uint8Array(hashBuffer))
+    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+
+    // Prepend to get full signature
+    const fullSignature = `v0=${hashHex}`
+    const slackSignature = request.headers.get('X-Slack-Signature')
+
+    return fullSignature === slackSignature;
+  } catch (err) {
+    return false;
+  }
+}
+
 /**
  * Handles shortening links. Expects the text to look like:
  * `/shorten <path> <url>`, and result in `vhl.ink/path`
  */
 export default async (request: Request) => {
 	try {
+    if (!(await validSlackRequest(request))) {
+      throw new Error("Request did not come from Slack.")
+    }
+
 		// https://api.slack.com/tutorials/slash-block-kit is more updated than the Cloudflare tutorial
 		const formData = await request.formData();
-		const text = formData.get('text');
+    const text = formData.get('text');
 
 		// Ensure that we process a valid string
 		if (typeof text === 'string') {
