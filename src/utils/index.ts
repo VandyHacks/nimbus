0f202903dc4b5b913ba6fb655832756568d46ec4
Gabriel Ting <gabrielfting@gmail.com>
2021-01-16 10:55:26 -0600

    Refactor request verification using importKey

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -15,55 +15,34 @@ export const validSlackRequest = async (request: Request): Promise<boolean> => {
 		const sigBasestring = `v0:${timestamp}:${requestBody}`;
 
 		// Hash the basestring using signing secret as key, taking hex digest of hash. Uses Cloudflare's Web Crypto https://developers.cloudflare.com/workers/runtime-apis/web-crypto
-		const msgUint8 = new TextEncoder().encode(sigBasestring);
+
+		const encoder = new TextEncoder();
+		const msgUint8 = encoder.encode(sigBasestring);
         // Refer to https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest#converting_a_digest_to_a_hex_string
         
         const key = await crypto.subtle.importKey(
             "raw",
-            SLACK_SIGNING_SECRET,
+            encoder.encode(SLACK_SIGNING_SECRET),
             { name: "HMAC", hash: "SHA-256"},
             false,
             ["verify"]
-        )
+		)
 
-        // const expirationMs = 60000
-        // const expiry = Date.now() + expirationMs
-        // const dataToAuthenticate = url.pathname + expiry
+		const signatureHeader = request.headers.get('X-Slack-Signature')?.split("=");
+		const version = signatureHeader?.[0]
+		const slackSignature = signatureHeader?.[1]
 
-        const encoder = new TextEncoder();
+		if (version && slackSignature) {
+			const signatureUint8 = encoder.encode(slackSignature)
+			// We want to verify that hte slack signature matches what we hash with the key we made
+			const result = await crypto.subtle.verify({name: "HMAC", hash: "SHA-256"}, key, signatureUint8, msgUint8);
+			console.log(slackSignature)
+			return result;
+		}
 
-        const mac = await crypto.subtle.sign("HMAC", key, encoder.encode(dataToAuthenticate))
-
-        // `mac` is an ArrayBuffer, so we need to jump through a couple of hoops to get
-        // it into a ByteString, and then a Base64-encoded string.
-        const base64Mac = btoa(String.fromCharCode(...new Uint8Array(mac)))
-
-		const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);
-		const hashArray = Array.from(new Uint8Array(hashBuffer));
-		const hashHex = hashArray
-			.map(b => b.toString(16).padStart(2, '0'))
-			.join('');
-
-		// Prepend to get full signature
-		const fullSignature = `v0=${hashHex}`;
-		const slackSignature = request.headers.get('X-Slack-Signature');
-		console.log(slackSignature);
-        console.log(fullSignature);
-        
-        const hmac = crypto.createHmac('sha256', signingSecret);
-        const [version, hash] = signature.split('=');
-        hmac.update(`${version}:${ts}:${body}`);
-
-        // if (!timingSafeCompare(hash, hmac.digest('hex'))) {
-        // debug('request signature is not valid');
-        // throw errorWithCode(
-        //     new Error('Slack request signing verification failed'),
-        //     ErrorCode.SignatureVerificationFailure,
-        // );
-        // }
-
-		return fullSignature === slackSignature;
+		throw new Error('The signature was missing version or signature.')
 	} catch (err) {
+		console.log(err.message)
 		return false;
 	}
 };
